c216b1c720068e301b248235511cab8c925bf716
Nick Lázaro Camasca <edsonlc_007@hotmail.com>
2019-09-03 10:12:27 -0500

    Implementación Crt-Y

--- Memento.py
+++ Memento.py
@@ -8,7 +8,10 @@ class Memento():
         return self.color
         
 class Caretaker():
+
     def __init__(self, mementos):
+        """Input: list of mements"""
+
         self.mementos = mementos
 
     def _push_(self,memento):
@@ -27,7 +30,8 @@ class TopicApp():
         self.window.geometry('350x200')
         self.window.config(bg="#00FF00")
 
-        self.caretaker = Caretaker([Memento("#00FF00")])
+        self.caretakerZ = Caretaker([Memento("#00FF00")])
+        self.caretakerY = Caretaker([])
 
         btn_blue = Button(self.window, text="Blue", command = lambda : self.__change_color("#0000FF"))
         btn_blue.grid(column=0, row=0)
@@ -39,22 +43,28 @@ class TopicApp():
         btn_green.grid(column=0, row=2)
 
         btn_ctr_z = Button(self.window, text="Crt-z", command = lambda : self.__restore_memento())
-        btn_ctr_z.grid(column=3, row=3)
+        btn_ctr_z.grid(column=0, row=3)
 
+        btn_ctr_y = Button(self.window, text="Crt-y", command = lambda : self.__adelant_memento())
+        btn_ctr_y.grid(column=0, row=4)
+        
         self.window.mainloop()
+
 
     def __change_color(self, color):
         self.window.config(bg=color)
-        self.caretaker._push_(Memento(color))
-        print(self.caretaker.mementos)
-
+        self.caretakerZ._push_(Memento(color))
+        self.caretakerY = Caretaker([])
     
     def __restore_memento(self):
-        self.caretaker._pop_()
-        color = self.caretaker.mementos[-1].get_color()
-        print(self.caretaker.mementos)
-        self.window.config(bg = color)
-        
+        memento = self.caretakerZ._pop_()
+        self.caretakerY._push_(memento)
+        self.window.config(bg=memento.get_color())
+
+    def __adelant_memento(self):
+        memento = self.caretakerY._pop_()
+        self.caretakerZ._push_(memento)
+        self.window.config(bg=memento.get_color())
         
 if __name__=='__main__':
     Apx = TopicApp()
